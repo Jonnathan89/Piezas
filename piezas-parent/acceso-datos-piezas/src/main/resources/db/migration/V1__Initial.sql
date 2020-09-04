f391c54ce0d448771732a6f0671ef409ad9a7cb7
Jonnathan Ladino Hernandez <j.ladinoh@ext.santanderconsumer.co>
2020-09-04 17:58:40 -0500

    Se ajusta secript

--- piezas-parent/acceso-datos-piezas/src/main/resources/db/migration/V1__Initial.sql
+++ piezas-parent/acceso-datos-piezas/src/main/resources/db/migration/V1__Initial.sql
@@ -4,3 +4,17 @@ create table usuario(
    password varchar(100),
    typeuser varchar(50)
 );
+
+
+create table datamueblesaccesorios(
+   id serial primary key not null,   
+   nombre varchar(100),
+   primer_apellido varchar(100),
+   segundo_apellido varchar(100),
+   tipo_identificacion varchar(100),
+   identificacion integer not null,
+   fecha date,
+   numero_celular integer,
+   actividad_economica varchar(100),
+   ingresos_mensuales integer
+);
