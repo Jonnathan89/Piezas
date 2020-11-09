fa8e99427d2804929c499517c298596b19f52c3f
Jonnathan Ladino Hernandez <j.ladinoh@ext.santanderconsumer.co>
2020-11-09 17:02:43 -0500

    Se ajustan clases y configuracion Keyloack para M&A

--- piezas-parent/acceso-datos-piezas-client/src/main/java/co/com/santander/oauth/config/FeignRequestInterceptor.java
+++ piezas-parent/acceso-datos-piezas-client/src/main/java/co/com/santander/oauth/config/FeignRequestInterceptor.java
@@ -4,9 +4,11 @@
 package co.com.santander.oauth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import co.com.santander.springsecurity.Service.GenerateTokenService;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
 import lombok.extern.slf4j.Slf4j;
@@ -18,25 +20,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class FeignRequestInterceptor implements RequestInterceptor {
+	
+	
 	private static final String AUTHORIZATION_HEADER = "Authorization";
+
 	private static final String BEARER_TOKEN_TYPE = "Bearer";
-	private static final String USER_TOKEN = "feign";
-	
+
 	@Autowired
-	GenerateTokenService generateTokenService;
-	
+	OAuth2RestOperations restTemplate;
+
 	@Override
 	public void apply(RequestTemplate template) {
-		log.info("[FeignRequestInterceptor] start interceptor");
-		if(template.headers().containsKey(AUTHORIZATION_HEADER))
-			log.warn("The autorization token has been already set");
+		log.info("[OAuth2FeignRequestInterceptor] start interceptor");
+		if (template.headers().containsKey(AUTHORIZATION_HEADER)) 
+			log.warn("The Authorization token has been already set");
 		else {
-			log.info("[FeignRequestInterceptor] Constructing Header {} for Token {}", AUTHORIZATION_HEADER, BEARER_TOKEN_TYPE);
-			String token = generateTokenService.getJWTToken(USER_TOKEN);
-			template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE,token));
+			log.info("[OAuth2FeignRequestInterceptor] Constructing Header {} for Token {}", AUTHORIZATION_HEADER, BEARER_TOKEN_TYPE);
+			Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+			if (auth instanceof AnonymousAuthenticationToken) {
+				log.warn("Role is anonymous, no token will be created. Probably called service without security");
+			} else {
+				String authorization = restTemplate.getAccessToken().getValue();
+				template.header(AUTHORIZATION_HEADER, String.format("%s %s", BEARER_TOKEN_TYPE, authorization));
+			}
 		}
-		
-		
 	}
 
 }
